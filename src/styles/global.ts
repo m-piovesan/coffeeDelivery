3506b7834dd25f9037a83f542dd4d9de83a35b98
Matheus <matheusmoraespiovesan@gmail.com>
2024-01-09 16:13:38 -0300

    start - setting syledcomponents up

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    :focus {
+        outline: 0;
+    }
+
+    body {
+        background: ${props => props.theme.bg};
+        color: ${props => props.theme['base-text']};
+    }
+
+    body, input, button, textarea {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1rem;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-family: 'Baloo 2', sans-serif;
+    }
+`;
